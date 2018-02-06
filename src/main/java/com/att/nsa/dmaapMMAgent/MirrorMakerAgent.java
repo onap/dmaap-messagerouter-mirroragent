4db673812e589a129fcb56d0421f6cac09df73e2
Soumendu Sekhar Acharya <sa00498080@techmahindra.com>
2018-02-06 10:07:20 +0530

    sonar critical for Exception Handling
    Used a logger to log this exception
    Sonar Link:
    https://sonar.onap.org/component_issues/index?id=org.onap.dmaap.messagerouter.mirroragent%3AdmaapMMAgent#assignees=soumendu|resolved=false
    Location:
    src/main/java/com/att/nsa/dmaapMMAgent/MirrorMakerAgent.java
    Line No-175
    
    Change-Id: Id67043b7781e96c639688f12d51f175ef5b07bfb
    Issue-ID: DMAAP-230
    Signed-off-by: Soumendu Sekhar Acharya <sa00498080@techmahindra.com>

--- src/main/java/com/att/nsa/dmaapMMAgent/MirrorMakerAgent.java
+++ src/main/java/com/att/nsa/dmaapMMAgent/MirrorMakerAgent.java
@@ -176,6 +176,7 @@ public class MirrorMakerAgent {
 					input.close();
 				} catch (IOException e) {
 					e.printStackTrace();
+					logger.error(" IOException Occered " + e);
 				}
 			}
 			if (out != null) {
