bcf5bd04ea03bb4909a9de508ed0a90518457e5b
Dushyant Singh Thakur <dt00503496@techmahindra.com>
2018-02-06 15:39:54 +0530

    sonar major for error-handling
    Either log or rethrow this exception
    Sonar Link:
    https://sonar.onap.org/component_issues?id=org.onap.dmaap.messagerouter.mirroragent%3AdmaapMMAgent#resolved=false|assignees=dthakur
    Location:
    src/main/java/com/att/nsa/dmaapMMAgent/MirrorMakerAgent.java
    Line No-363
    
    Change-Id: Ia2d37cdc6ddf5489dcddb984aa4d5485d6d58849
    Issue-ID: DMAAP-237
    Signed-off-by: Dushyant Singh Thakur <dt00503496@techmahindra.com>

--- src/main/java/com/att/nsa/dmaapMMAgent/MirrorMakerAgent.java
+++ src/main/java/com/att/nsa/dmaapMMAgent/MirrorMakerAgent.java
@@ -181,7 +181,7 @@ public class MirrorMakerAgent {
 					out.close();
 				} catch (IOException e) {
 					e.printStackTrace();
-					logger.error("IOException" + e);
+					logger.error("Exception is : "+e);
 				}
 			}
 		}
@@ -361,7 +361,7 @@ public class MirrorMakerAgent {
 
 			}
 		} catch (Exception e) {
-			logger.error("Exception is : " +e);
+			logger.error("Exception at readAgentTopic : " + e);
 		}
 
 	}
@@ -385,7 +385,7 @@ public class MirrorMakerAgent {
 		} else if (exists == false && mirrorMakers == null) {
 			mirrorMakers = new ListMirrorMaker();
 			ArrayList<MirrorMaker> list = mirrorMakers.getListMirrorMaker();
-			list = new ArrayList<MirrorMaker>();
+			list = new ArrayList();
 			list.add(newMirrorMaker);
 			mirrorMakers.setListMirrorMaker(list);
 		}
