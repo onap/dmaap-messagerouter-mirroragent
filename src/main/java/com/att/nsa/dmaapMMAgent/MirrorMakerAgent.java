c640944531cb051e232f0855f7fb6d153b0d19b1
Snigdha Sucharita Patra <sp00503415@techmahindra.com>
2018-02-26 16:36:37 +0530

    sonar critical issue
    Either log or rethrow this exception
    Sonar Link:
    https://sonar.onap.org/project/issues?assignees=Snigdha503415&id=org.onap.dmaap.messagerouter.mirroragent%3AdmaapMMAgent&open=AV5H0sAO32hFUzlqdBu8&resolved=false&severities=CRITICAL
    Location:
    src/main/java/com/att/nsa/dmaapMMAgent/MirrorMakerAgent.java
    Line No-412
    
    Change-Id: I03d8b97a63653257b6d23804e5d5cd925f6c8580
    Issue-ID: DMAAP-314
    Signed-off-by: Snigdha Sucharita Patra <sp00503415@techmahindra.com>

--- src/main/java/com/att/nsa/dmaapMMAgent/MirrorMakerAgent.java
+++ src/main/java/com/att/nsa/dmaapMMAgent/MirrorMakerAgent.java
@@ -410,7 +410,7 @@ public class MirrorMakerAgent {
 				try {
 					out.close();
 				} catch (IOException e) {
-					e.printStackTrace();
+					logger.error(" IOException Occered ", e);
 				}
 			}
 		}
